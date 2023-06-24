89a4ec76164f49f2057c6e5745b43ab89246eea6
Lootiano <lucianograbin12@gmail.com>
2023-06-24 19:00:32 -0300

    creating splitString

---
+++ src/splitString.js
@@ -0,0 +1,7 @@
+const splitString = (num) => {
+  let number = num.split(",");
+  console.log(number);
+  return number;
+};
+
+module.exports = splitString;
--- src/subtraction.js
+++ src/subtraction.js
@@ -1,8 +1,10 @@
+const splitString = require("./splitString");
+
 const sub = (interface) => {
   interface.question(
     "Digite os dois números separados por uma virgula. Ex: 30,20: ",
     (num) => {
-      let number = num.split(",");
+      let number = splitString(num);
       let num1 = Number(number[0]);
       let num2 = Number(number[1]);
       let res = num1 - num2;
